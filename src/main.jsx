f19e70a8a743e19e0fb196ca66d6b43aa5c990f7
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-21 17:29:21 +0300

    finished lesson

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { store } from './store.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Store State Change Listener
+const render = () => {
+  root.render(
+    <React.StrictMode>
+      <App state={store.getState()} dispatch={store.dispatch} />
+    </React.StrictMode>
+  );
+}
+
+render();
+
+// Subscribe to state changes
+store.subscribe(render);
---
+++ src/store.jsx
@@ -0,0 +1,24 @@
+import { createStore } from 'redux';
+
+// Action Creators
+export function increment() { 
+  return {type: 'increment'}
+}
+
+export function decrement() { 
+  return {type: 'decrement'}
+}
+
+// Reducer / Store
+const initialState = 0;
+const countReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'increment':
+      return state + 1; 
+    case 'decrement':
+      return state - 1; 
+    default:
+      return state;
+  }
+};  
+export const store = createStore(countReducer);
